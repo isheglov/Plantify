35e825b93c4b070f7b379d11b0cd86afa3cbee9c
i.shcheglov <ilia.shcheglov@westwing.de>
2021-03-04 01:14:35 +0100

    added users

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,22 +4,40 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
+use App\Repository\GardenRepository;
 use Symfony\Component\HttpFoundation\Response;
 
 final class UserController
 {
-    public function index(Request $request): Response
+    /** @var GardenRepository */
+    private $gardenRepository;
+
+    public function __construct(
+        GardenRepository $gardenRepository
+    ) {
+        $this->gardenRepository = $gardenRepository;
+    }
+
+    public function index(): Response
     {
-//        var_dump($request->toArray());
+        $gardenList = $this->gardenRepository->findAll();
+
+        if(empty($gardenList)) {
+            return new Response(
+                '<html>
+                        <body>
+                            <div><a href="/garden/create">Создание участка</a></div>
+                            <div><a href="/planning">Планирование посадок(в разработке)</a></div>
+                        </body>
+                    </html>'
+            );
+        }
+
         return new Response(
             '<html>
                         <body>
-                            <div><a href="/garden/create">Создание участка</a></div>
                             <div><a href="/garden">К участку</a></div>
-                            <div><a href="/planning">Планирование посадок</a></div>
-                            '. ' '/*print_r($request->toArray(),1)*/.'
-                            <div>Посадка растения</div>
+                            <div><a href="/planning">Планирование посадок(в разработке)</a></div>
                         </body>
                     </html>'
         );
