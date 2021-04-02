267182afd40e6e83f680d563da72ebd8ee4fb191
i.shcheglov <ilia.shcheglov@westwing.de>
2021-04-02 16:13:30 +0200

    Added migration for history

---
+++ migrations/Version20210402133039.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210402133039 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE history_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE history (id INT NOT NULL, cell_id INT NOT NULL, plant_id INT NOT NULL, planted_from DATE DEFAULT NULL, planted_to DATE DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_27BA704BCB39D93A ON history (cell_id)');
+        $this->addSql('CREATE INDEX IDX_27BA704B1D935652 ON history (plant_id)');
+        $this->addSql('ALTER TABLE history ADD CONSTRAINT FK_27BA704BCB39D93A FOREIGN KEY (cell_id) REFERENCES garden_cell (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE history ADD CONSTRAINT FK_27BA704B1D935652 FOREIGN KEY (plant_id) REFERENCES plant (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE history_id_seq CASCADE');
+        $this->addSql('DROP TABLE history');
+    }
+}
