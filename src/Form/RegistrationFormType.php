b20ea563e22f0fdc197ccc86eb53d1d2b7f89c9b
i.shcheglov <ilia.shcheglov@westwing.de>
2021-05-02 22:14:27 +0200

    Localized sign in form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -20,6 +20,7 @@ class RegistrationFormType extends AbstractType
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+                'label' => 'я не буду вредить',
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
@@ -36,7 +37,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Пароль должен быть не менее {{ limit }} знаков',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
